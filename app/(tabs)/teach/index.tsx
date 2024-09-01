7bcdd7bfd07cf160503fcc3b64c076cc8834d43e
Nechama Haschel <chumie@Live.com>
2024-09-01 07:20:13 -0400

    Updated

--- app/(tabs)/teach/index.tsx
+++ app/(tabs)/teach/index.tsx
@@ -11,7 +11,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 function NewGroup() {
   const navigation = useNavigation();
 
-  // State variables for each form field
   const [name, setName] = useState('');
   const [startDate, setStartDate] = useState('');
   const [weeks, setWeeks] = useState('');
